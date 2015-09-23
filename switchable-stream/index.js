7d589703540412e3668b5d2027b99d28f92a01d6
John Hiesey <john@hiesey.com>
2015-09-22 18:15:00 -0700

    revert switchable stream to original implementation

--- switchable-stream/index.js
+++ switchable-stream/index.js
@@ -10,25 +10,17 @@ Add sequence numbers and framing
 
 var duplexify = require('duplexify')
 var inherits = require('inherits')
-var MultiStream = require('multistream')
+var through2 = require('through2')
+
 
 var SwitchableStream = module.exports = function (initialStream) {
 	var self = this
 	duplexify.call(self)
 
-	self._currentStream = null
-	self._replacements = []
-	self._waiting = []
+	self._inBuffer = null
 
-	self._inStream = new MultiStream(function (cb) {
-		if (self._replacements.length)
-			cb(null, self._replacements.shift())
-		else
-			self._waiting.push(cb)
-	})
-	self.setReadable(self._inStream)
-
-	self.replace(initialStream)
+	if (initialStream)
+		self.replace(initialStream)
 }
 
 inherits(SwitchableStream, duplexify)
@@ -36,14 +28,127 @@ inherits(SwitchableStream, duplexify)
 SwitchableStream.prototype.replace = function (newStream) {
 	var self = this
 
-	if (self._waiting.length)
-		self._waiting.shift()(null, newStream)
+	self._switchingTo = newStream
+
+	if (!self._out) {
+		self._switchWrite()
+		self._switchRead()
+		return
+	}
+
+	// send replaceWrite
+	var header = new Buffer(1)
+	header.writeUInt8(1)
+	self._out.push(header)
+
+	if (self._gotReplaceWrite)
+		self._switchWrite()
+}
+
+SwitchableStream.prototype._switchWrite = function () {
+	var self = this
+
+	// if actually switching
+	if (self._out) {
+		// send replaceRead
+		var header = new Buffer(1)
+		header.writeUInt8(2)
+		self._out.push(header)
+	}
+
+	self._out = through2(function (chunk, enc, cb) {
+		// console.log('pushing')
+		self._outFilter(this, chunk, enc, cb)
+	})
+
+	self._switchedDuplex = duplexify()
+	self._switchedDuplex.setReadable(self._out)
+	self._switchedDuplex.pipe(self._switchingTo).pipe(self._switchedDuplex)
+
+	// console.log('changing to new stream')
+	self.setWritable(self._out)
+
+	self._gotReplaceWrite = false
+	self._switchingTo = null
+}
+
+SwitchableStream.prototype._switchRead = function () {
+	var self = this
+	var actuallySwitching = !!self._in
+	self._in = through2(function (chunk, enc, cb) {
+		self._inFilter(this, chunk, enc, cb)
+	})
+
+	self._switchedDuplex.setWritable(self._in)
+	self.setReadable(self._in)
+
+	if (actuallySwitching) {
+		self.emit('switched')
+	}
+}
+
+SwitchableStream.prototype._outFilter = function (stream, chunk, enc, cb) {
+	var self = this
+
+	// Add 5 bytes
+	var header = new Buffer(5)
+	header.writeUInt8(0, 0)
+	header.writeUInt32BE(chunk.length, 1)
+	stream.push(header)
+	stream.push(chunk)
+
+	cb()
+}
+
+SwitchableStream.prototype._inFilter = function (stream, chunk, enc, cb) {
+	var self = this
+
+	var buf
+	if (self._inData)
+		buf = Buffer.concat([self._inData, chunk])
 	else
-		self._replacements.push(newStream)
+		buf = chunk
 
-	self.setWritable(newStream)
+	while (true) {
+		if (buf.length) {
+			self._inData = buf
+		} else {
+			self._inData = null
+			cb()
+			return
+		}
 
-	if (self._currentStream)
-		self._currentStream.end()
-	self._currentStream = newStream
+		var msgType = buf.readUInt8(0)
+		switch(msgType) {
+			case 0:
+				if (buf.length < 5) {
+					cb()
+					return
+				}
+				var len = buf.readUInt32BE(1)
+				if (buf.length < len + 5) {
+					cb()
+					return
+				}
+				stream.push(buf.slice(5, len + 5))
+				buf = buf.slice(len + 5)
+				break
+
+			case 1:
+				buf = buf.slice(1)
+				self._gotReplaceWrite = true
+				if (self._switchingTo)
+					self._switchWrite()
+				break
+
+			case 2:
+				self._switchRead()
+				// This should always be the last data on this stream
+				self._inData = null
+				cb()
+				return
+			default:
+				cb(new Error('Unexpected message type:', msgType))
+		}
+	}
 }
