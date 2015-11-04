b355b7a1ef58b3e420e53c3c3f749bd03b221787
John Hiesey <john@hiesey.com>
2015-11-03 16:29:53 -0800

    Basic lookup working

--- index.js
+++ index.js
@@ -10,31 +10,31 @@ console.log('my id:', id)
 
 var overlay
 if (typeof window === 'undefined') {
-	overlay = new Overlay(id, [], 8085)
+	overlay = new Overlay(id, 8085, [])
 }
 else {
-	overlay = new Overlay(id, ['ws://localhost:8085'])
+	overlay = new Overlay(id, null, ['ws://localhost:8085'])
 	window.overlay = overlay
 }
 
-overlay.on('stream', function (err, id, str) {
-	console.log('incoming stream with id:', id)
+overlay.on('stream', function (id, name, str) {
+	console.log('incoming stream from node with id:', id, 'name:', name)
 	global.stream = str
 	str.on('data', function (data) {
 		console.log(id + ':', data.toString())
 	})
 })
 
-global.createStream = function (node, id) {
+global.createStream = function (id, name) {
 	console.log('outgoing stream with id:', id)
-	overlay.sendStream(node, id, function (err, str) {
+	overlay.openStream(id, name, {}, function (err, strs) {
 		if (err) {
-			console.error('failed to create stream with id:', id)
+			return console.error(err)
 		}
+		var str = strs[0]
 		global.stream = str
 		str.on('data', function (data) {
 			console.log(id + ':', data.toString())
 		})
 	})
-
 }
